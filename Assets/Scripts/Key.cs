cbd1dc59dacc438e704593476cc9855ef42a3d9b
LucasP0326 <2002lucassanchez@gmail.com>
2023-03-05 00:55:52 -0500

    I forgot what I'm committing but it's stuff 1
    Yeahhhh...I forgor

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     public Component doorcolliderhere;
+    public Component doorcolliderhere2;
+    public Component doorcolliderhere3;
     public GameObject keygone;
 
     // Update is called once per frame
     void OnTriggerStay ()
     {
         if(Input.GetKey(KeyCode.E))
-        doorcolliderhere.GetComponent<BoxCollider> ().enabled = true;
+        {
+            doorcolliderhere.GetComponent<BoxCollider> ().enabled = true;
+            doorcolliderhere2.GetComponent<BoxCollider> ().enabled = true;
+            doorcolliderhere3.GetComponent<BoxCollider> ().enabled = true;
+        }
 
         if(Input.GetKey(KeyCode.E))
         keygone.SetActive(false);
