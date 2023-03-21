a8c91be70119f0e1f2ae54c2ce343cc427c92711
PrimeMods <kwmax@protonmail.com>
2023-03-21 17:18:41 -0400

    Invisibility and tweaks

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -28,9 +28,13 @@ public class Enemy : MonoBehaviour
     //attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
-    public GameObject projectile;
+    public int attackDamage;
 
     Animator animator;
+
+    bool invisibled;
+
+    bool alive = true;
 
     void Start()
     {
@@ -43,7 +47,25 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         //Test if player is in fov
-        FieldOfViewCheck();
+        if (!player.GetComponent<Player>().invisible && alive)
+        {
+            invisibled = false;
+            FieldOfViewCheck();
+        }
+        else if (!invisibled && alive)
+        {
+            StopAllCoroutines();
+            seenPlayer = false;
+            seesPlayer = false;
+            if (waypoints.Length > 0) ChooseWaypoint();
+            else 
+            {
+                agent.SetDestination(transform.position);
+                animator.ResetTrigger("Walk");
+                animator.SetTrigger("Idle");
+            }
+            invisibled = true;
+        }
 
         if (seenPlayer) //if player has been seen lately
         {
@@ -52,6 +74,9 @@ public class Enemy : MonoBehaviour
             Vector3 newPos = transform.position - dirToPlayer;
             agent.SetDestination(newPos);
             animator.SetTrigger("Walk");
+
+            float distance = Vector3.Distance (transform.position, player.transform.position);
+            if (distance <= 1) AttackPlayer();
         }
         else
         {
@@ -72,7 +97,7 @@ public class Enemy : MonoBehaviour
 
     void FieldOfViewCheck()
     {
-        //Is the player within view distance?
+        //Is the player within view distance, and doens't have invisibility active?
         Collider[] rangeChecks = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
         if (rangeChecks.Length != 0)
         {
@@ -138,13 +163,10 @@ public class Enemy : MonoBehaviour
 
         if (!alreadyAttacked)
         {
-            Debug.Log("attack");
-            ///Attack code here
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            //Debug.Log("attack!");
             animator.SetTrigger("Attack");
-            ///End of attack code
+            player.GetComponent<Player>().health -= attackDamage;
+            if (player.GetComponent<Player>().health == 0) player.GetComponent<Player>().Death();
 
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
@@ -163,6 +185,10 @@ public class Enemy : MonoBehaviour
     }
     private void DestroyEnemy()
     {
-        Destroy(gameObject);
+        agent.SetDestination(transform.position);
+        seesPlayer = false;
+        seenPlayer = false;
+        animator.SetTrigger("Death");
+        alive = false;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    bool hasGun;
+    public bool hasGun;
+    public int health;
+    
+    //pickups
+    bool standingOnPickup;
+    GameObject pickupUnderPlayer;
+    public bool invisible;
+    public int invisibilityLength;
+
     public GameObject gunPosition;
+    public Material transparentMat;
+    public Material opaqueMat;
+    public GameObject Mesh;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && standingOnPickup)
+        {
+            standingOnPickup = false;
+            Destroy(pickupUnderPlayer);
+            playerInvisible();
+            invisible = true;
+            StartCoroutine(InvisibilityCountdown());
+        }
+    }
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Gun" && !hasGun)
@@ -18,5 +41,66 @@ public class Player : MonoBehaviour
             collision.gameObject.GetComponent<ProjectileGun>().equipped = true;
             hasGun = true;
         }
+
+        if (collision.gameObject.tag == "Invisibility")
+        {
+            standingOnPickup = true;
+            pickupUnderPlayer = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "Invisibility")
+        {
+            standingOnPickup = false;
+            pickupUnderPlayer = null;
+        }
+    }
+
+    private IEnumerator InvisibilityCountdown()
+    {
+        yield return new WaitForSeconds(invisibilityLength - 3);
+        
+        playerInvisible();
+        yield return new WaitForSeconds(0.5f);
+        playerVisible();
+        yield return new WaitForSeconds(0.5f);
+        playerInvisible();
+        yield return new WaitForSeconds(0.5f);
+        playerVisible();
+
+        yield return new WaitForSeconds(0.25f);
+        playerInvisible();
+        yield return new WaitForSeconds(0.25f);
+        playerVisible();
+        yield return new WaitForSeconds(0.25f);
+        playerInvisible();
+        yield return new WaitForSeconds(0.25f);
+        playerVisible();
+        yield return new WaitForSeconds(0.25f);
+        playerInvisible();
+        yield return new WaitForSeconds(0.25f);
+        playerVisible();
+        
+        invisible = false;
+    }
+
+    private void playerInvisible()
+    {
+        Mesh.GetComponent<SkinnedMeshRenderer>().materials[0].CopyPropertiesFromMaterial(transparentMat);
+        Mesh.GetComponent<SkinnedMeshRenderer>().materials[1].CopyPropertiesFromMaterial(transparentMat);
+        Mesh.GetComponent<SkinnedMeshRenderer>().materials[2].CopyPropertiesFromMaterial(transparentMat);
+    }
+    private void playerVisible()
+    {
+        Mesh.GetComponent<SkinnedMeshRenderer>().materials[0].CopyPropertiesFromMaterial(opaqueMat);
+        Mesh.GetComponent<SkinnedMeshRenderer>().materials[1].CopyPropertiesFromMaterial(opaqueMat);
+        Mesh.GetComponent<SkinnedMeshRenderer>().materials[2].CopyPropertiesFromMaterial(opaqueMat);
+    }
+
+    public void Death()
+    {
+        Debug.Log("You died!");
     }
 }
